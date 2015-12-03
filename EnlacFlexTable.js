1b76f651e693d817adfc0755ec75243c4e78f19e
Enlac <calneshippuden@gmail.com>
2015-12-03 16:45:53 -0200

    Update EnlacFlexTable.js

--- EnlacFlexTable.js
+++ EnlacFlexTable.js
@@ -26,7 +26,7 @@ function generateTable(){
   var tbodyHeight = $('#tbody').height();
   //var tableWidth = theadWidth > tbodyWidth?theadWidth:tbodyWidth;
   //var tableHeight = theadHeight > tbodyHeight?theadHeight:tbodyHeight;
-  var tablePadding = Math.sqrt(parseInt($('#table').css('padding'), null));
+  var tablePadding = Math.sqrt(parseInt($('#table').css('padding'), 10));
   
   $('#thead').css({display:'inline-flex', width:'calc(100% - '+tablePadding+'px)'});
   $('#table').css({overflow:'hidden'});
@@ -39,21 +39,21 @@ function generateTable(){
   var viewPortH = $(window).height();
   var viewPortW = $(window).width();
   if($('#table').attr('height') !== '' && $('#table').attr('height') !== undefined){
-	var tH = parseInt($('#table').attr('height'));
-	var tableHS = parseInt($('#table').css('paddingTop'))+parseInt($('#table').css('paddingBottom'))+parseInt($('#table').css('borderTopWidth'))+parseInt($('#table').css('borderBottomWidth'))+parseInt($('#table').css('marginTop'))+parseInt($('#table').css('marginBottom'));
+	var tH = parseInt($('#table').attr('height'),10);
+	var tableHS = parseInt($('#table').css('paddingTop'),10)+parseInt($('#table').css('paddingBottom'),10)+parseInt($('#table').css('borderTopWidth'),10)+parseInt($('#table').css('borderBottomWidth'),10)+parseInt($('#table').css('marginTop'),10)+parseInt($('#table').css('marginBottom'),10);
 	$('#table').height(viewPortH*(tH*0.01)-tableHS);
   }
   if($('#table').attr('width') !== '' && $('#table').attr('width') !== undefined){
-    var tW = parseInt($('#table').attr('width'));
-	var tableWS = parseInt($('#table').css('paddingLeft'))+parseInt($('#table').css('paddingRight'))+parseInt($('#table').css('borderLeftWidth'))+parseInt($('#table').css('borderRightWidth'))+parseInt($('#table').css('marginLeft'))+parseInt($('#table').css('marginRight'));
+    var tW = parseInt($('#table').attr('width'),10);
+	var tableWS = parseInt($('#table').css('paddingLeft'),10)+parseInt($('#table').css('paddingRight'),10)+parseInt($('#table').css('borderLeftWidth'),10)+parseInt($('#table').css('borderRightWidth'),10)+parseInt($('#table').css('marginLeft'),10)+parseInt($('#table').css('marginRight'),10);
 	$('#table').width(viewPortW*(tW*0.01)-tableWS);
   }  
 
   //DETECT LIMIT OF TBODY
   var theadH = $('#thead').height();
-  var theadS = parseInt($('#thead').css('borderTopWidth'))+parseInt($('#thead').css('borderBottomWidth'))+parseInt($('#thead').css('paddingTop'))+parseInt($('#thead').css('paddingBottom'));
+  var theadS = parseInt($('#thead').css('borderTopWidth'),10)+parseInt($('#thead').css('borderBottomWidth'),10)+parseInt($('#thead').css('paddingTop'),10)+parseInt($('#thead').css('paddingBottom'),10);
   var tbodyH = $('#tbody')[0].scrollHeight;
-  var tbodyS = parseInt($('#tbody').css('borderTopWidth'))+parseInt($('#tbody').css('borderBottomWidth'))+parseInt($('#tbody').css('paddingTop'))+parseInt($('#tbody').css('paddingBottom'));
+  var tbodyS = parseInt($('#tbody').css('borderTopWidth'),10)+parseInt($('#tbody').css('borderBottomWidth'),10)+parseInt($('#tbody').css('paddingTop'),10)+parseInt($('#tbody').css('paddingBottom'),10);
   var tableH = (($('#table').height()-theadH)-theadS)-tbodyS;
   
   var tbodyOverflow = (tableH-tbodyH);
@@ -65,7 +65,7 @@ function generateTable(){
   //SCROLLING SYSTEM |MEN AT WORK|
   $('#tbody').css({overflowY:'auto'}); 
   var scrollingBarWidth =  $('#tbody').width()-document.getElementById('tbody').clientWidth;
-  var theadSpace = Number(parseInt($('#thead').css('borderLeftWidth'))+parseInt($('#thead').css('borderRightWidth'))+parseInt($('#thead').css('padding')));
+  var theadSpace = Number(parseInt($('#thead').css('borderLeftWidth'),10)+parseInt($('#thead').css('borderRightWidth'),10)+parseInt($('#thead').css('padding'),10));
   theadSpace = isNaN(theadSpace) === true ? 0 : theadSpace;
   
   var theadWidthInPx = $('#thead').width();
